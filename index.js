49e9ac278c801a4db90555062e7030937edd7add
LegendYt4k <139982135+LegendYt4k@users.noreply.github.com>
2023-08-27 13:39:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { spawn } = require('child_process');
+const express = require('express')
+const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(8080)
 const ffmpegCommand = 'ffmpeg -stream_loop -1 -re -i neffex.mp4 -stream_loop -1 -re -i https://stream-152.zeno.fm/ez4m4918n98uv -c:v libx264 -pix_fmt yuvj420p -maxrate 512k -preset ultrafast -r 12 -framerate 1 -g 48 -crf 23 -c:a aac -b:a 96k -ar 44100 -strict -2 -video_track_timescale 100 -b:v 500k -s 1280x720 -f flv rtmp://a.rtmp.youtube.com/live2/xxr2-pf89-k1c0-egz6-f0y2';
 
 const ffmpegProcess = spawn(ffmpegCommand, {
